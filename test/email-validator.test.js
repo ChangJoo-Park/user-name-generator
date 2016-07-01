a8ea9c8e02e2dbe749737763456c31a60321701d
ChangJoo Park <pcjpcj2@gmail.com>
2016-07-01 12:08:50 +0200

    Test email address length, change simpler regex.

--- test/email-validator.test.js
+++ test/email-validator.test.js
@@ -12,7 +12,14 @@ suite('EmailValidator', function() {
       assert.equal(splittedEmail.domain, 'gmail.com');
     });
   });
-
+  /*
+    http://www.ex-parrot.com/~pdw/Mail-RFC822-Address.html
+    http://emailregex.com/
+    Start with alphabet and numbers, special character only  '-','_'
+    domain part is not case sensitive.
+    NOTE: is better?
+    "simple email validation" -> "send activate email to user" -> "activate"
+  */
   suite('#validate', function() {
     test('should return false empty email', function() {
       assert.isNotTrue(EmailValidator.validate(''));
@@ -26,6 +33,24 @@ suite('EmailValidator', function() {
       assert.isNotTrue(EmailValidator.validate('john..doe@example.com'));
       assert.isNotTrue(EmailValidator.validate('john.doe@example..com'));
       assert.isNotTrue(EmailValidator.validate('a@a'));
+    });
+
+    test('should local part start with a-z, A-Z, 0-9.', function() {
+      assert.isNotTrue(EmailValidator.validate('!pcjpcj2@gmail.com'));
+      assert.isNotTrue(EmailValidator.validate('+pcjpcj2@gmail.com'));
+      assert.isNotTrue(EmailValidator.validate('~pcjpcj2@gmail.com'));
+    });
+
+    test('should domain part has lower case.', function() {
+      assert.isNotTrue(EmailValidator.validate('pcjpcj2@Gmail.com'));
+      assert.isNotTrue(EmailValidator.validate('pcjpcj2@NAVER.COM'));
+      assert.isTrue(EmailValidator.validate('pcjpcj2@naver.com'));
+    });
+
+    test('should domain part does not have more than 3 parts.', function() {
+      assert.isTrue(EmailValidator.validate('pcjpcj2@gmail.com'));
+      assert.isTrue(EmailValidator.validate('pcjpcj2@gmail.co.kr'));
+      assert.isNotTrue(EmailValidator.validate('pcjpcj2@gmail.co.kr.not.kr'));
     });
   });
 });
